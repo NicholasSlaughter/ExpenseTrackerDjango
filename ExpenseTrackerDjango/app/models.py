59294f1d1cd9494036fec298b3000605303d15f2
NicholasSlaughter <38059177+NicholasSlaughter@users.noreply.github.com>
2021-04-13 14:29:30 -0700

    Added user foreign key to Expense and Alert models

--- ExpenseTrackerDjango/app/models.py
+++ ExpenseTrackerDjango/app/models.py
@@ -3,6 +3,7 @@ Definition of models.
 """
 
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
 #class ApplicationUser(models.Model):
@@ -22,7 +23,7 @@ class Expense(models.Model):
     amount = models.FloatField(max_length=20,null=False)
     date = models.DateTimeField(null=False)
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
-    #application_user = models.ForeignKey(ApplicationUser,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,default=0)
 
 class Period(models.Model):
     name = models.CharField(max_length=50,null=False)
@@ -38,4 +39,4 @@ class Alert(models.Model):
     period_end_date = models.DateTimeField(null=False,default="1111-11-11 11:11")
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     period = models.ForeignKey(Period, on_delete=models.CASCADE)
-    #application_user = models.ForeignKey(ApplicationUser,on_delete=models.CASCADE)
+    user = models.ForeignKey(User,on_delete=models.CASCADE,default=0)
